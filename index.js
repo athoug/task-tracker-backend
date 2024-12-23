d1b2b7e0a034264b8d92060e7055aaea5d62de41
Athoug <athoug.s@gmail.com>
2024-12-23 14:22:15 +0300

    setup user schema

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const connect = require('./connect');
 const User = require('./models/user');
 
-const connectionURL = 'mongodb://localhost:27017/test';
+const connectionURL = 'mongodb://localhost:27017/tasko';
 connect(connectionURL)
 	.then(async (connection) => {
 		console.log('app connected to database - ' + connection);
 
-		const silence = new User({ name: 'silence' });
-		await silence.save();
-		silence.speak();
+		const athoug = new User({ name: 'athoug', email: 'athoug@gmail.com' });
+		await athoug.save();
 
-		// retrieve all kittens
-		const kittens = await User.find({});
-		console.log('kittens:', kittens);
-
-		// find only kittens named silence
-		const silent = await User.find({ name: /^sil/ });
-		console.log('silent kittens:', silent);
+		console.log(athoug.name, athoug.email);
 	})
 	.catch((err) => console.error(err));
